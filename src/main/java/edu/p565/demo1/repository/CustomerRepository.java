1e7a09f11a3f824ff605317440842473cbcb3e79
BlytheHammett <95008523+BlytheHammett@users.noreply.github.com>
2023-03-30 18:01:13 -0400

    Add files via upload

---
+++ src/main/java/edu/p565/demo1/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package edu.p565.demo1.repository;
+
+import edu.p565.demo1.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
